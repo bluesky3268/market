cc036dd915e7e01652ff6f84ec87c6cacdc67c18
HYUNBIN_CHO <bluesky3268@gmail.com>
2021-05-04 10:19:03 +0900

    조현빈 : 매핑 정보 수정

--- src/main/java/korit/market/controller/ItemController.java
+++ src/main/java/korit/market/controller/ItemController.java
@@ -1,12 +1,25 @@
 package korit.market.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
 
+    @GetMapping("/list")
+    public String tent_Category() {
+        return "productlist";
+    }
 
+    @GetMapping("/listdetail")
+    public String tent_Category_detail() {
+        return "productlistdetail";
+    }
 
+    @GetMapping("/itemdetail")
+    public String item_detail() {
+        return "itemsViewPage";
+    }
 }
