0f2a8050ded91b547bee51b0a7004bf968dceef0
HELee9614 <80077170+HELee9614@users.noreply.github.com>
2021-05-12 17:37:48 +0900

    하은 : CartRepository 에 Orders 잘못 입력한 부분 수정해씀니다... 뎨둉해요 ;-;

--- src/main/java/korit/market/Repository/CartRepository.java
+++ src/main/java/korit/market/Repository/CartRepository.java
@@ -1,10 +1,10 @@
 package korit.market.Repository;
 
 
-import korit.market.entity.Orders;
+import korit.market.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CartRepository extends JpaRepository<Orders, Long>, JpaSpecificationExecutor<Orders> {
+public interface CartRepository extends JpaRepository<Cart, Long>, JpaSpecificationExecutor<Cart> {
 
 }
